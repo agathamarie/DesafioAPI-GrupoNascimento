f54e47638d35e5f8bf7f95081eef6034b2f0cd9d
AbnerWender <abnderwender@gmail.com>
2025-02-06 13:03:26 -0200

    card notícias

---
+++ templates/cardNoticia.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cardNoticia.css">
+    <title>cardNotícia</title>
+</head>
+<body>
+    <section class="container">
+        <div id="imagem-noticia">
+            <img src="../img/image-teste.png" alt="">
+        </div>
+
+        <div id="manchete-noticia">
+            <p>Cracolândia, Nhanhá teve 'campinho da morte' e agora jorra disputa de tráfico</p>
+        </div>
+
+        <div id="data-noticia">
+            <p>06/02/2025</p>
+        </div>
+
+    </section>
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -3,19 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/header.css">
     <link rel="logo-api.png" href="favicon.ico" type="image/x-icon">
     <title>header</title>
 </head>
 <header id="">
     <div id="logo">
-        <img src="logo-api.png" alt="logo aqui">
+        <img src="./img/logo-api.png" alt="logo aqui">
     </div>
     <div id="div-btn-busca">
-        <button id="btn-busca">BUSCA</button>
-
+        <button id="btn-busca" src=""><strong>BUSCA</strong>
+            <img src="./img/procurar.png" alt="lupa pesquisa" id="lupa">
+        </button>
     </div>
 </header>
 <body>
 </body>
+<script src="script.js"></script>
 </html>
